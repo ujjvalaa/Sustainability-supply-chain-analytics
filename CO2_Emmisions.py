b15b4cab877247227cb44e9604278e61a4a51d60
ujjvalaa <73737145+ujjvalaa@users.noreply.github.com>
2023-11-08 15:54:54 -0500

    CO2_emmisions.py

--- CO2_Emmisions.py
+++ CO2_Emmisions.py
@@ -20,3 +20,55 @@ COLS_JOIN = ['Warehouse Code', 'Customer Code']
 df_join = df_join.merge(df_dist, on=COLS_JOIN, how='left')
 
 print("{:,} records".format(len(df_join)))
+
+
+# Read Unit of Measure Conversions data
+df_uom = pd.read_csv('Data/uom_conversions.csv', index_col=0)
+print("{:,} Unit of Measure Conversions".format(len(df_uom))
+
+# Create a copy of df_lines to join with uom data
+df_join = df_lines.copy()
+
+# Define columns for joining records
+COLS_JOIN = ['Item Code']
+
+# Merge df_join with df_uom
+df_join = df_join.merge(df_uom, on=COLS_JOIN, how='left')
+
+# Drop any redundant columns
+df_join.drop(df_join.filter(regex='_y$').columns.tolist(), axis=1, inplace=True)
+print("{:,} records".format(len(df_join))
+df_join.head()
+
+
+# Calculate Weight (KG)
+df_join['KG'] = df_join['Units'] * df_join['Conversion Ratio']
+
+# Define columns to group by for aggregation
+GPBY_ORDER = ['Date', 'Month-Year', 
+    'Warehouse Code', 'Warehouse Name', 'Warehouse Country', 'Warehouse City',
+    'Customer Code', 'Customer Country', 'Customer City', 'Location', 'GPS 1', 'GPS 2', 
+    'Road', 'Rail', 'Sea', 'Air',
+    'Order Number']
+
+# Aggregate data
+df_agg = df_join.groupby(GPBY_ORDER)[['Units', 'KG']].sum().reset_index()
+
+# CO2 Emissions Calculation
+dict_co2e = {'Air': 2.1, 'Sea': 0.01, 'Road': 0.096, 'Rail': 0.028}
+MODES = ['Road', 'Rail', 'Sea', 'Air']
+
+for mode in MODES:
+    df_agg['CO2 ' + mode] = df_agg['KG'] / 1000 * df_agg[mode] * dict_co2e[mode]
+
+df_agg['CO2 Total'] = df_agg[['CO2 ' + mode for mode in MODES]].sum(axis=1)
+
+# Mapping the delivery Mode
+df_agg['Delivery Mode'] = df_agg[MODES].apply(
+    lambda t: [mode if t[mode] > 0 else '-' for mode in MODES], axis=1)
+
+dict_map = dict(zip(df_agg['Delivery Mode'].astype(str).unique(), 
+    [i.replace(", '-'", '').replace("'-'", '').replace("'", '') for i in df_agg['Delivery Mode'].astype(str).unique()]))
+
+df_agg['Delivery Mode'] = df_agg['Delivery Mode'].astype(str).map(dict_map)
+
